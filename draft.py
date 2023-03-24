ee5176848695f0f9a1d31940fae79bf02f11d588
23031251 <zd230219@gmail.com>
2023-03-24 13:01:15 +0700

    23032406 work with draft

--- draft.py
+++ draft.py
@@ -1,6 +1,21 @@
 spisok = [2, 3, 5, 7, 11, 13]
+stroka = '2, 3, 5, 7, 11, 13'
 # print(spisok[-1])
 # string = '2, 3, 5, 7, 11, 13'
 # chars = list(string)
 # print(chars)
-print(spisok[::-3])
+# print(spisok[::-1])
+# print(len(spisok))
+# spisok.append(15)
+a = [50, 60]
+# spisok.append(a)
+# print(spisok)
+b = '2slovo'
+# spisok.extend(b)
+# print(spisok)
+# s
+# spisok.extend(b)
+# spisok.extend(c)
+# spisok.insert(0, a)
+spisok[-2] = 20
+print(spisok)
