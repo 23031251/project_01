7f32ae3a7b6c5138faf5fea35b0d975cf296496f
23031251 <zd230219@gmail.com>
2023-03-26 02:49:51 +0700

    23032512 work with draft

--- draft.py
+++ draft.py
@@ -1,5 +1,6 @@
 spisok = [2, 3, 5, 7, 11, 13]
 stroka = '2, 3, 5, 7, 11, 13'
+matrica = [ [4,5] , [7,9] ]
 # print(spisok[-1])
 # string = '2, 3, 5, 7, 11, 13'
 # chars = list(string)
@@ -19,6 +20,21 @@ b = '2slovo'
 # spisok[-2] = 20
 # spisok.remove(2)
 # del spisok[1:3]
-del stroka[1:3]
-# print(spisok)
-print(stroka)
+# del stroka[1:3]
+spisok2 = spisok
+spisok3 = spisok * 3
+stroka *= 2
+# print('id spisok:  ', id(spisok))
+# print('id spisok2: ', id(spisok2))
+# print('id spisok3: ', id(spisok3))
+# print('spisok3: ', spisok3)
+# print('spisok3 is spisok: ', spisok3 is spisok[:])
+# print('spisok3 == spisok: ', spisok3 == spisok[:])
+# print('spisok2 is spisok: ', spisok2 is spisok)
+# print('spisok2 == spisok: ', spisok2 == spisok)
+# print(list('abcdefg'))
+# print(list(range(5, 160, 10)))
+# print(list(range(0, 5)))
+# print(list(spisok2))
+x = (1, 2, 3, 4, 5)
+print(list(x))
